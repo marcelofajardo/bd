de424884e07024509ac99b019a64573695a6c07a
marcelofajardo <marcelo.fajardo@gmail.com>
2016-09-27 16:01:56 -0300

    base de datos

--- pruebas_menu_forms/conexion.php
+++ pruebas_menu_forms/conexion.php
@@ -1,5 +1,11 @@
 <?php
-	$mysqli=new mysqli("localhost","root","sistemas","tienda"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
+$server="localhost";	
+$user="u812363560_agen";
+$bd="u812363560_agen";
+$clave="sistemas999";
+	
+$mysqli=new mysqli($server,$user,$clave,$bd);
+//$mysqli=new mysqli("localhost","root","sistemas","tienda"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
 	
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
--- pruebas_menu_forms/tabla3.php
+++ pruebas_menu_forms/tabla3.php
@@ -1,7 +1,7 @@
 <?php
 	require 'conexion.php';
 	
-	$sql = "SELECT dni, nombre, apellido, tipo_dni_id_tp, descripcion  FROM personas inner join  tipo_dni on tipo_dni_id_tp=codigo";
+	$sql = "SELECT dni, nombre, apellido, tipo_dni_id_tp, descripcion  FROM personas left join  tipo_dni on tipo_dni_id_tp=codigo";
 	$result=$mysqli->query($sql);
 	
 ?>
@@ -18,26 +18,28 @@
 			background-color: #0f0; <!--Color de fondo Verde-->
 			}
 		</style>
-		
+		<link rel="stylesheet" type="text/css" href="tabla2.css" media="screen">
 	</head>
 	
 	<body>
 		<div align="center">
-			<table border="1" width="80%">
-				<tr>
-					<th width="10%">DNI</th>
+			<table class="table1" border="1" width="100%" cellspacing=0>
+				<thead>
+                                <tr>
+					<th width="20%">DNI</th>
+					<th width="40%">Apellido</th>
 					<th width="30%">Nombre</th>
-					<th width="30%">Apellido</th>
 					<th width="10%">Tipo DNI</th>
 
 				</tr>
+                                </thead>
 				<?php while($row = $result->fetch_assoc()){ 
 
 				?>
 
 					<td><?php echo $row['dni']; ?></td>
-					<td><?php echo $row['nombre']; ?></td>
 					<td><?php echo $row['apellido']; ?></td>
+					<td><?php echo $row['nombre']; ?></td>
 					<td><?php echo $row['descripcion']; ?></td>
 					
 				</tr>
@@ -46,4 +48,5 @@
 		</div>
 	</body>
 </html>
+
 
